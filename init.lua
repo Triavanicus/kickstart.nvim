c4750b8deae14c2723d409b3f43f90ddf241cd86
Triavanicus <triavanicus@gmail.com>
2024-01-09 12:38:02 -0600

    Changed lualine to have separators

--- init.lua
+++ init.lua
@@ -208,13 +208,13 @@ require('lazy').setup({
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
-        theme = 'edge',
-        component_separators = '|',
-        section_separators = '',
+        icons_enabled = true,
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
       },
     },
   },
