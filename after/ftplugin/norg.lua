bf3c4967e904a7dc71aeea11c50ca1620c9212b5
Triavanicus <triavanicus@gmail.com>
2024-01-28 12:21:43 -0600

    Added settings specific to norg files

---
+++ after/ftplugin/norg.lua
@@ -0,0 +1,6 @@
+vim.opt_local.colorcolumn = ""
+vim.opt_local.spell = true
+vim.opt_local.linebreak = true
+vim.opt_local.spelllang = "en"
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
